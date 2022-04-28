32098d13e72f8316f7dac145079863d28f334a49
Milan Lagae <milan.lagae@student.howest.be>
2022-04-28 21:18:55 +0200

    Created cardservice, retrieve multiple cards from db

---
+++ Library/Services/CardService.cs
@@ -0,0 +1,31 @@
+﻿using mtg_lib.Library.Models;
+
+
+namespace mtg_lib.Library.Services
+{
+    public class CardService
+    {
+        private mtgdevContext context;
+        
+        
+        public CardService() {
+            context = new mtgdevContext();
+        }
+        
+        public IEnumerable<Card> GetCards() {
+            return context.Cards.ToList();
+        }
+
+        
+        public IEnumerable<Card> GetSetAmountOfCards(int amount)
+        {
+            if (amount != 0)
+            {
+                return GetCards().Take(amount);
+            }
+            return GetCards().Take(100);
+        }
+
+        
+    }    
+}
