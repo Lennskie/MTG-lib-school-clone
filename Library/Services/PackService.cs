1b0b180b889ed296542391f12ed2dbf35ff92493
Milan Lagae <milan.lagae@student.howest.be>
2022-05-01 19:38:51 +0200

    Retrieve cards from database and create a fake pack

---
+++ Library/Services/PackService.cs
@@ -0,0 +1,34 @@
+﻿using mtg_lib.Library.Models;
+
+
+namespace mtg_lib.Library.Services
+{
+    public class PackService
+    {
+        private mtgdevContext context;
+        private CardService cardService = new CardService();
+
+
+        public PackService()
+        {
+            context = new mtgdevContext();
+        }
+
+        public IEnumerable<Card> CreateRandomPack(string rarity)
+        {
+            IEnumerable<Card> cards = cardService.GetCards();
+            
+            // 15 Cards
+            var r = new Random();
+
+            IEnumerable<Card> cardPacks = cards.OrderBy(c => r.Next(cards.Count() - 1)).Take(15);
+
+
+            return cardPacks;
+        }
+
+
+
+    }
+}
+
