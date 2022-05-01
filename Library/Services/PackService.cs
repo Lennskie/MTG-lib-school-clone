eb0593ebe05b5389933322389e7a25be1f109adb
Milan Lagae <milan.lagae@student.howest.be>
2022-05-01 20:18:56 +0200

    Updated function for creating fake card packs

--- Library/Services/PackService.cs
+++ Library/Services/PackService.cs
@@ -20,11 +20,39 @@ namespace mtg_lib.Library.Services
             
             // 15 Cards
             var r = new Random();
+            
+            // 1 Basic Land
+            IEnumerable<Card> basicCard = cards.Where(c => c.Type.Contains("Land")).OrderBy(c =>  r.Next(cards.Count() - 1)).Take(1);
+            Console.WriteLine("Count: " + basicCard.Count());
+            
+            // 10 Commons
+            IEnumerable<Card> commonCards = cards.Where(c => c.RarityCode == "C").OrderBy(c =>  r.Next(cards.Count() - 1)).Take(10);
+            Console.WriteLine("Count: " + commonCards.Count());
+            
+            // 3 Uncommon
+            IEnumerable<Card> uncommonCards = cards.Where(c => c.RarityCode == "U").OrderBy(c =>  r.Next(cards.Count() - 1)).Take(3);
+            Console.WriteLine("Count: " + uncommonCards.Count());
+            
+            // 1 is rare or mythic
+            IEnumerable<Card> rareOrMythicCard = cards.Where(c => c.RarityCode is "R" or "M").OrderBy(c => r.Next(cards.Count() - 1)).Take(1);
+            Console.WriteLine("Count: " + rareOrMythicCard.Count());
 
+            
             IEnumerable<Card> cardPacks = cards.OrderBy(c => r.Next(cards.Count() - 1)).Take(15);
-
+            
+            /*IEnumerable<Card> cardPacks = basicCard;
+            cardPacks.Concat(commonCards);
+            cardPacks.Concat(uncommonCards);
+            cardPacks.Concat(rareOrMythicCard);*/
 
             return cardPacks;
+        }
+
+        private IEnumerable<Card> appendToList(IEnumerable<Card> toAppend)
+        {
+            IEnumerable<Card> toReturn = new List<Card>();
+
+            return toReturn;
         }
 
 
